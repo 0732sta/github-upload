a62c65e2960fa73e196fe9fece723333e014d5f9
Atsilah Anuar <42581091+0732sta@users.noreply.github.com>
2021-02-01 18:14:19 +0800

    Update library.sql

--- library.sql
+++ library.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 01, 2021 at 10:49 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `library`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `membership`
---
 
 CREATE TABLE `membership` (
   `id` int(11) NOT NULL,
